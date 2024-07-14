074dd8a29c015a344b1266a0fe6e654751a992a7
mthtitumir <titumirgccc@gmail.com>
2024-07-14 16:44:47 +0600

    special section responsive done

--- src/components/homepage/Chef/Chef.tsx
+++ src/components/homepage/Chef/Chef.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 import SubHeading from "../Header/SubHeading";
 
 const Chef = () => (
-  <div className="bg-black c-auto md:p-24">
-    <div className="flex flex-col md:flex-row items-center ">
-      <div className="w-full md:w-1/3 mb-8 md:mb-0 flex justify-end">
+  <div className="c-auto p-8 md:py-24">
+    <div className="flex flex-col md:flex-row items-center gap-8 md:gap-0">
+      <div className="w-full md:w-1/3 flex justify-end">
         <Image
           src={images.chef}
           alt="chef_image"
@@ -14,27 +14,27 @@ const Chef = () => (
           objectFit="cover"
         />
       </div>
-      <div className="w-full md:w-2/3 flex flex-col pl-20 gap-3">
+      <div className="w-full md:w-2/3 flex flex-col md:pl-20 gap-3">
         <div>
           <SubHeading title="Chef's world" />
-          <h1 className="text-4xl font-bold text-white mb-4">
+          <h1 className="text-2xl md:text-4xl font-semibold mb-4">
             What we believe in
           </h1>
         </div>
-        <div className="w-full md:mr-12">
-          <div className="flex items-end mb-8">
+        <div className="w-full md:mr-12 text-justify">
+          <div className="flex items-start gap-2 mb-2">
             <Image
               src={images.quote}
               alt="quote_image"
               width={47}
               height={40}
             />
-            <p className="text-white text-lg ml-2">
+            <p className="md:text-lg">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor
               sit.
             </p>
           </div>
-          <p className="text-white text-lg">
+          <p className="md:text-lg">
             auctor sit iaculis in arcu. Vulputate nulla lobortis mauris eget
             sit. Nulla scelerisque scelerisque congue ac consequat, aliquam
             molestie lectus eu. Congue iaculis integer curabitur semper sit
@@ -42,10 +42,10 @@ const Chef = () => (
           </p>
         </div>
         <div className="flex flex-col">
-          <div className="text-yellow-500 text-2xl font-normal mr-4 mb-4 md:mb-0">
+          <div className="text-yellow-500 text-2xl font-normal ">
             Kevin Luo
           </div>
-          <p className="text-white text-lg">Chef & Founder</p>
+          <p className="text-white md:text-lg">Chef & Founder</p>
           <Image
             src={images.sign}
             alt="sign_image"
